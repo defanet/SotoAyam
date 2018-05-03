d5e60cc8cf18ee75dbb23831c71b29a5bb5229b4
defanet <defanet@gmail.com>
2018-05-03 12:12:27 +0800

    Update Exc_Functional.java

--- src/JQC/Exc_Functional.java
+++ src/JQC/Exc_Functional.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package JQC;
 
 import Function.Mainfunction;
@@ -11,7 +6,7 @@ import java.util.HashMap;
 
 /**
  *
- * @author Agung Danu Wijaya
+ * @author Agung Danu Wijaya, Farhamsa D
  */
 public class Exc_Functional {
 
